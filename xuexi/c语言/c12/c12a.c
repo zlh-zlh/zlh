29fb2e54b4f4888c16b3a927a491a3a32c054bd0
zlh-zlh <54268193+zlh-zlh@users.noreply.github.com>
2020-03-22 16:03:28 +0800

    Add files via upload

---
+++ xuexi/c语言/c12/c12a.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int ch;
+    while((ch =getchar()) !='\n' )
+    {
+        if(ch=='C')
+        {
+            continue;
+        }
+        putchar(ch);
+    }
+    putchar('\n');
+    return 0;
+}
